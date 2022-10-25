3661f42e82ebf0d3a5087ab66b01c2dfd3ab0ba7
DavidP1996erea <david.perea@iesnervion.es>
2022-10-25 23:07:01 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio8_Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Ejercicio8_Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace Ejercicio8_Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+
+            return View();  
+        }
+
+
+
+        public ActionResult Saludo(String nombre)
+        {
+           
+            String saludar = "Buenas " + nombre ;
+
+            ViewBag.saludar = saludar;
+
+            return View();
+        }
+
+    }
+}
